147b3020c6695a5f65bd5c8acd03a4d4041a8407
Joaquin <juacocampero@gmail.com>
2017-10-02 21:01:31 -0300

    first coomit

---
+++ getRecipe.js
@@ -0,0 +1,14 @@
+/**
+ * main.js
+ */
+const scrapeIt = require("scrape-it");
+
+module.exports = function (url, cb) {
+
+    const conf = {
+
+    };
+
+    // Callback interface
+    scrapeIt(`https://cookpad.com/ar/buscar/${q}`, conf, callback);
+}
